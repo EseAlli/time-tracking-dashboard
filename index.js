4fbc1716d2ad9400fe02e2c11851e8bdc3277221
EseAlli <essealli.ea@gmail.com>
2022-11-11 06:13:24 +0100

    implemented switch between stats

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const toogleList = Array.from(document.querySelectorAll("li"));
-console.log(toogleList);
+const cards = Array.from(document.querySelectorAll(".card"));
+let time = "weekly";
+let data;
+
+async function getData() {
+  let response = await fetch("./data.json");
+  data = await response.json();
+  updateDOM(time);
+}
+
+getData();
 
 toogleList.forEach((item) => {
   item.addEventListener("click", function () {
     let current = document.getElementsByClassName("active");
-    console.log(current);
     current[0].className = current[0].className.replace("active", "");
     this.className += "active";
+    time = this.innerHTML.toLowerCase();
+    updateDOM(time);
   });
 });
+
+function updateDOM(time) {
+  cards.forEach((card) => {
+    let cardTitleTag = card.getElementsByClassName("title").item(0);
+    let cardTitle = cardTitleTag.innerHTML;
+    let current = card.getElementsByClassName("current").item(0);
+    let previous = card.getElementsByClassName("previous-hr").item(0);
+    let previousTag = card.getElementsByClassName("previous").item(0);
+    let [cardInfo] = data.filter((d) => d.title === cardTitle);
+    let { timeframes } = cardInfo;
+    current.firstChild.nodeValue = timeframes[time].current;
+    previous.innerHTML = `${timeframes[time].previous}hrs`;
+
+    switch (time) {
+      case "weekly":
+        previousTag.firstChild.nodeValue = "Last Week";
+        break;
+      case "daily":
+        previousTag.firstChild.nodeValue = "Yesterday";
+        break;
+      case "monthly":
+        previousTag.firstChild.nodeValue = "Last Month";
+        break;
+    }
+  });
+}
